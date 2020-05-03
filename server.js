c76b2ab473adf595639d04d8e4ad58634cab57a9
Will Huynh <william.huynh12@gmail.com>
2020-05-03 17:57:00 +1000

    heroku deploy

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 //Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(__dirname, '/build/index.html');
+        res.sendFile(path.join(__dirname, 'client/build/index.html'));
     })
 }
 
