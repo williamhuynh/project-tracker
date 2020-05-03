7ac64ccdffc28b50dfaecd81ac908cbab07b3569
Will Huynh <william.huynh12@gmail.com>
2020-05-03 23:19:29 +1000

    nav bar styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import ProjectsList from "./components/projects-list.component";
@@ -9,16 +8,27 @@ import EditProject from "./components/edit-project.component";
 import CreateProject from "./components/create-project.component";
 import CreateUser from "./components/create-user.component";
 
+function TopBar(){
+  return (
+    <div className="top-bar">
+      <span className="title">Project Tracker App</span>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <Router>
       <div className="container">
         <Navbar />
-        <br />
-        <Route path="/" exact component={ProjectsList} />
-        <Route path="/edit/:id" component={EditProject} />
-        <Route path="/create" component={CreateProject} />
-        <Route path="/user" component={CreateUser} />
+        <TopBar />
+        <div className="main-container">
+          <Route path="/" exact component={ProjectsList} />
+          <Route path="/edit/:id" component={EditProject} />
+          <Route path="/create" component={CreateProject} />
+          <Route path="/user" component={CreateUser} />
+        </div>
       </div>
     </Router>
   );
--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,29 +1,105 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as UserIcon } from '../svg/user-solid.svg';
+import { ReactComponent as PlusIcon } from '../svg/plus-solid.svg';
+import { ReactComponent as HomeIcon } from '../svg/home-solid.svg';
+import axios from 'axios';
+
+
+
+function NavItem(props) {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <li className="nav-item">
+            <a href={props.link} className="icon-button" onClick={() => setOpen(!open)}>
+                {props.icon}
+            </a>
+
+            {open && props.children}
+        </li>
+    );
+}
+
+
+function DropDownMenu() {
+
+    function DropDownItem(props){
+        return (
+            <a href="#" className="menu-item">
+                <span className="icon-button">{props.icon}</span>
+                {props.children}
+            </a>
+        );
+    }
+
+    return (
+        <div className="dropdown">
+            <DropDownItem>My Profile</DropDownItem>
+            <DropDownItem>Settings</DropDownItem>
+            <DropDownItem>Logout</DropDownItem>
+        </div>
+    );
+}
+
+function ProjectList(props){
+    return (
+        <li className="nav-item">
+            <a href={props.id} className="icon-button">
+                {props.icon}
+            </a>
+        </li>
+    );
+}
 
 export default class Navbar extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {projects: []};
+    }
+
+    componentDidMount() {
+        axios.get('/projects/')
+            .then(response => {
+                this.setState({projects: response.data})
+            })
+    }
     
+
+    ProjectItem(){
+        return this.state.projects.map(p => (
+            <ProjectList id={p._id} icon={p.name} />
+        ))
+    
+    }
+    
+
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/" className="navbar-brand">Navbar</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <nav className="navbar">
+                <ul className="navbar-nav">
+                    <NavItem link="/" icon={<HomeIcon />}></NavItem>
+                    { this.ProjectItem() }   
+                    {/* <li className="nav-item active">
+                        <Link to="/" className="nav-link">Projects</Link>
+                    </li> */}
+                    <NavItem link="create" icon={<PlusIcon />}></NavItem>
+                    {/* <NavItem link="user" icon="New User"></NavItem> */}
+                    <NavItem link="#" icon={<UserIcon />}>
+                        <DropDownMenu />
+                    </NavItem>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link to="/" className="nav-link">Projects</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/create" className="nav-link">Add New Project</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/user" className="nav-link">Create User</Link>
-                        </li>
-                    </ul>
-                 </div>
+                    {/* <li className="nav-item active">
+                        <Link to="/create" className="nav-link">Add New Project</Link>
+                    </li>
+                    
+                    <li className="nav-item active">
+                        <Link to="/user" className="nav-link">Create User</Link>
+                    </li> */}
+                </ul>
             </nav>
 
         )
